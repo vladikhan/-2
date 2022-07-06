9d04e901e7557a454c9517e86c964937d6b5b4ef
vladikhan <84037014+vladikhan@users.noreply.github.com>
2022-07-07 04:42:53 +0900

    1

---
+++ index.js
@@ -0,0 +1,11 @@
+let myName = "Vladi";
+    console.log(myName);
+let mySurname = "Khan";
+    console.log(mySurname);
+const myFavoriteDrink = "Hibiki";
+    console.log(myFavoriteDrink);
+const myFavoriteAnimal = "Dog";
+    console.log(myFavoriteAnimal);
+let myFavoriteProgrammingLanguage = "JS";
+    console.log(myFavoriteProgrammingLanguage);
+
